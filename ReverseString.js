0febab26bf602fd943355799c7fda6f5a0de8279
Akshay Gaikwad <45968153+realakshay@users.noreply.github.com>
2024-10-05 14:51:33 +0000

    update on reverse string

--- ReverseString.js
+++ ReverseString.js
@@ -1,10 +1,10 @@
 function reverseString(str) {
-    let revString = "";
-    for (let index = str.length - 1; index >= 0; index--) {
-      const element = str.charAt(index);
-      revString = revString + element;
-    }
-    return str === revString;
+  let revString = "";
+  for (let index = str.length - 1; index >= 0; index--) {
+    const element = str.charAt(index);
+    revString = revString + element;
   }
-  str = "abcba";
-  console.log(reverseString(str));
+  return str === revString;
+}
+str = "abcba";
+console.log(reverseString(str));
